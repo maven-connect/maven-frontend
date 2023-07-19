a71fd77313827fa43a7fbb17edff7c60588381c5
Om Thakare <omthakare1606@gmail.com>
2023-07-19 15:50:24 +0530

    fixed verify page with mantine ui

--- src/components/VerifyPage.js
+++ src/components/VerifyPage.js
@@ -1,8 +1,18 @@
-import {fetchProfile} from "@/features/profileSlice";
-import {verifyProfile} from "@/pages/api/api";
-import {Center, Flex, Select} from "@mantine/core";
-import React, {useState} from "react";
-import {Toaster, toast} from "react-hot-toast";
+import { fetchProfile } from "@/features/profileSlice";
+import { verifyProfile } from "@/pages/api/api";
+import {
+  Anchor,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Paper,
+  Select,
+  Text,
+  Title,
+} from "@mantine/core";
+import React, { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
 const branches = [
   {
@@ -44,40 +54,47 @@ export default function VerifyPage() {
   return (
     <>
       <Toaster></Toaster>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 items-center">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <Container size={420} my={40}>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <Title
+            align="center"
+            sx={(theme) => ({
+              fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+              fontWeight: 500,
+            })}
+          >
             Complete your Profile
-          </h2>
-        </div>
-        <Select
-          mt={50}
-          label="Branch"
-          w={320}
-          data={branches}
-          onChange={(val) => setSelectedBranch(val)}
-        />
-        <Select
-          mt={25}
-          label="Batch"
-          w={320}
-          data={["2018", "2019", "2020", "2021", "2022"]}
-          onChange={(val) => setBatch(val)}
-        />
-        <button
-          onClick={() => {
-            verifySubmit();
-          }}
-          className="flex w-80 mt-10 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Submit
-        </button>
-      </div>
+          </Title>
+          <Text color="dimmed" size="sm" align="center" mt={5}>
+            Select the branch and the batch below
+          </Text>
+
+          <form>
+            <Select
+              mt={10}
+              label="Branch"
+              w={320}
+              data={branches}
+              onChange={(val) => setSelectedBranch(val)}
+            />
+            <Select
+              mt={25}
+              label="Batch"
+              w={320}
+              data={["2018", "2019", "2020", "2021", "2022"]}
+              onChange={(val) => setBatch(val)}
+            />
+            <Button
+              mt={10}
+              onClick={() => {
+                verifySubmit();
+              }}
+            >
+              Submit
+            </Button>
+          </form>
+        </Paper>
+      </Container>
     </>
   );
 }
